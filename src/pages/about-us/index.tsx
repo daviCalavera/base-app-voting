2544c7c04e73193b407bcd37a3f6ea5d426bc033
David <david77@Davids-MacBook-Air.local>
2023-07-19 00:27:09 -0300

    Add docker-compose local development

---
+++ src/pages/about-us/index.tsx
@@ -0,0 +1,15 @@
+import { NextPage } from "next";
+
+const AboutUs : NextPage = () => {
+  return (
+    <div className={""}>
+      <main className={""}>
+        <h1 className={""}>
+          About Us Example Page
+        </h1>
+      </main>
+    </div>
+  )
+}
+
+export default AboutUs;
